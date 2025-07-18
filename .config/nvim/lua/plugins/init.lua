df37aff632a272a3707f7dd2ab0104aaada9f420
nulln0ne <nulln0ne@protonmail.com>
2025-07-18 08:57:45 +0300

    feat: add tmux

---
+++ .config/nvim/lua/plugins/init.lua
@@ -0,0 +1,72 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    event = "BufWritePre", -- uncomment for format on save
+    opts = require "configs.conform",
+  },
+
+  -- These are some examples, uncomment them if you want to see them work!
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require "configs.lspconfig"
+    end,
+  },
+
+  -- test new blink
+  -- { import = "nvchad.blink.lazyspec" },
+
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "vim",
+        "lua",
+        "vimdoc",
+        "go",
+        "rust",
+        "typescript",
+        "javascript",
+      },
+    },
+  },
+
+  {
+    "ray-x/go.nvim",
+    dependencies = { -- optional packages
+      "ray-x/guihua.lua",
+      "neovim/nvim-lspconfig",
+      "nvim-treesitter/nvim-treesitter",
+    },
+    opts = {
+      -- lsp_keymaps = false,
+      -- other options
+      lsp_cfg = {
+        settings = {
+          hints = {
+            assignVariableTypes = true,
+            compositeLiteralFields = true,
+            constantValues = true,
+            functionTypeParameters = true,
+            parameterNames = true,
+            rangeVariableTypes = true,
+          },
+        },
+      },
+    },
+    config = function(lp, opts)
+      require("go").setup(opts)
+      local format_sync_grp = vim.api.nvim_create_augroup("GoFormat", {})
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        pattern = "*.go",
+        callback = function()
+          require("go.format").goimports()
+        end,
+        group = format_sync_grp,
+      })
+    end,
+    event = { "CmdlineEnter" },
+    ft = { "go", "gomod" },
+    build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
+  },
+}
--- .zshrc
+++ .zshrc
@@ -1,19 +1,29 @@
+ZSH_THEME="agnoster" # set by `omz`
+
 export ZSH="$HOME/.oh-my-zsh"
-ZSH_THEME="fishy"
+export ZSH_THEME="fishy"
+
+export GOPATH="$HOME/go"
+
+export PATH="$GOPATH/bin:$PATH"
+export PATH="/opt/homebrew/opt/postgresql@17/bin:$PATH"
+export PATH="/opt/homebrew/opt/coreutils/libexec/gnubin:$PATH"
+export PATH="/opt/homebrew/opt/llvm/bin:$PATH"
+export PATH="/Users/hinko/.bun/bin:$PATH"
+
 plugins=(
   git
+  extract
+  docker
+  docker-compose
   zsh-syntax-highlighting
   zsh-autosuggestions
 )
-source $ZSH/oh-my-zsh.sh
-export GOPATH=$HOME/go
-export PATH="$GOPATH/bin:$PATH"
-export PATH="/opt/homebrew/opt/postgresql@17/bin:$PATH"
-eval "$(zoxide init zsh)"
-alias cd='z'
 
-[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
+source "$ZSH/oh-my-zsh.sh"
 
-export PATH="/opt/homebrew/opt/coreutils/libexec/gnubin:$PATH"
-
+alias cd="z"
 alias ls="ls --color=always"
+
+eval "$(zoxide init zsh)"
+[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
